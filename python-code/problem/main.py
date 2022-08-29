846bc758a8f36946bbbce7195e561669d2787ee4
Dmutro4347 <dmutrobui@gmail.com>
2022-08-29 13:07:49 +0100

    fisrt

--- python-code/problem/main.py
+++ python-code/problem/main.py
@@ -1,8 +1,2 @@
-b = []
-
-with open('/usr/share/doc/picom/picom.conf.example', 'r') as fl:
-    for i in fl.readlines():
-        if i[0] != '#':
-            print(i)
 for i in range(1, 11):
-    print(123)
+  print(i)
