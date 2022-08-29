4374d2034e6a66428f503a4b0b20203f508aa47e
Dmutro4347 <dmutrobui@gmail.com>
2022-08-29 17:16:00 +0100

    commit

--- python-code/problem/model.py
+++ python-code/problem/model.py
@@ -1,15 +1,14 @@
 import json
+import subprocess as sp
 
 task_list_read = open('/home/arfors/.config/code/python-code/problem/json/task-list.json', 'r')
 try:
-  task_list = json.loads(task_list_read.read())
+  task_list = json.load(task_list_read)
 except Exception as ex_:
   task_list_write = open('/home/arfors/.config/code/python-code/problem/json/task-list.json', 'w')
-  task_list_write.write('{}')
+  task_list_write.write('{"":""}')
+  task_list_write.close()
   task_list = json.loads(task_list_read.read())
-
-  
-task_list_write = open('/home/arfors/.config/code/python-code/problem/json/task-list.json', 'w')
 
 def get_task_list():
   for i in task_list:
@@ -20,13 +19,15 @@ def last_id():
   return int(task_list_keys[-1][0])
 
 def new_task(task_name, task_description):
+  task_list_write = open('/home/arfors/.config/code/python-code/problem/json/task-list.json', 'w')
   task_list[f'{last_id() + 1} - {task_name}'] = task_description
   json.dump(task_list, task_list_write)
+  task_list_write.close()
 
 def execute_task(task_id):
   for i in task_list:
-    if task_list[0] == task_id:
-      print(task_list.get(i))
+    if i[0] == task_id:
+      sp.call(task_list.get(i), shell=False)
 
 if __name__ == '__main__':
   execute_task('2')
